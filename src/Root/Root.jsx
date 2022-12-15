8925cbb437b88fb40cbf82b32f5415fcae91363a
Vovk.Mark <marekmark22@gmail.com>
2022-12-15 01:00:19 +0100

    Rebuild Organization page (20%/100%)

--- src/Root/Root.jsx
+++ src/Root/Root.jsx
@@ -4,26 +4,21 @@ import Home from "../pages/home/Home";
 import Organization from "../pages/organization/Organization";
 import Volunteer from "../pages/volunteer/Volunteer";
 import Photos from "../pages/photos/Photos";
-import { CssBaseline, ThemeProvider } from "@mui/material";
-import { default_theme } from "../modules/shared/components/Theme";
 import Test from "../pages/test/Test";
 import { BackgroundBox } from "../modules/shared/components/BackgroundBox";
 
 function App() {
   return (
-    <ThemeProvider theme={default_theme}>
-      <CssBaseline />
-      <BackgroundBox>
-        <ResponsiveAppBar />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="Organization" element={<Organization />} />
-          <Route path="Volunteer" element={<Volunteer />} />
-          <Route path="Photos" element={<Photos />} />
-          <Route path="Test" element={<Test />} />
-        </Routes>
-      </BackgroundBox>
-    </ThemeProvider>
+    <BackgroundBox>
+      <ResponsiveAppBar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="Organization" element={<Organization />} />
+        <Route path="Volunteer" element={<Volunteer />} />
+        <Route path="Photos" element={<Photos />} />
+        <Route path="Test" element={<Test />} />
+      </Routes>
+    </BackgroundBox>
   );
 }
 
--- src/pages/test/Test.jsx
+++ src/pages/test/Test.jsx
@@ -5,89 +5,162 @@ import Container from "@mui/material/Container";
 import TextField from "@mui/material/TextField";
 import { Grid } from "@mui/material";
 import Footer from "../../modules/shared/components/Footer";
+import { LineBox } from "../../modules/shared/components/LineBox";
+import Stack from "@mui/material/Stack";
 
 export default function Test() {
   return (
     <Box>
-      <Box
-        sx={{
-          pt: 8,
-          pb: 8,
-        }}
-      >
-        <Container maxWidth="s">
+      <LineBox>
+        <Box
+          sx={{
+            flexDirection: "column",
+            display: "flex",
+            marginRight: "25px",
+          }}
+        >
           <Typography
             component="h1"
-            variant="h1"
-            align="center"
-            color="text.primary"
-            fontSize="75px"
+            fontFamily="Nunito"
+            align="left"
+            color="#1E1E1E"
+            fontSize="60px"
             gutterBottom
-            fontWeight="700"
+            fontWeight="800"
           >
             ВІДКРИЙ СВІТ ВОЛОНТЕРСТВА!
           </Typography>
           <Typography
+            fontFamily="Nunito"
             variant="h1"
-            align="center"
-            color="text.secondary"
+            align="left"
             fontSize="30px"
             paragraph
-            fontWeight="300"
+            fontWeight="600"
+            color="#1E1E1E"
           >
             Сотні можливостей та однодумців вже чекають на тебе
           </Typography>
-        </Container>
+        </Box>
+        <Box
+          style={{
+            width: "300px",
+            height: "300px",
+            borderRadius: "100%",
+            backgroundColor: "black",
+            opacity: 1,
+            backgroundImage:
+              "radial-gradient(circle, rgba(235,235,235,1) 0%, rgba(255,255,255,1) 100%)",
+          }}
+        >
+          <Box
+            style={{
+              marginLeft: "7px",
+              marginTop: "15px",
+              width: "280px",
+              height: "280px",
+              borderRadius: "100%",
+              backgroundColor: "black",
+              opacity: 1,
+              backgroundImage:
+                "repeating-linear-gradient(45deg, #bfc6e3 25%, transparent 25%, transparent 75%, #bfc6e3 75%, #bfc6e3), repeating-linear-gradient(45deg, #bfc6e3 25%, #000000 25%, #000000 75%, #bfc6e3 75%, #bfc6e3)",
+              backgroundPosition: "0 0, 23px 23px",
+              backgroundSize: "46px 46px",
+            }}
+          />
+        </Box>
+      </LineBox>
+      <Box sx={{
+            width: "100%",
+            justifyContent: "center",
+            display: "flex",
+
+          }}>
+        <Box
+          sx={{
+            width: "65%",
+            backgroundImage: "linear-gradient(180deg, rgba(191,198,227,0) 0%, rgba(191,198,227,1) 20%, rgba(191,198,227,1) 80%, rgba(191,198,227,0) 100%)",
+            justifyContent: "center",
+            display: "flex",
+            borderRadius:"25px",
+            marginTop:"25px",
+            marginBottom:"25px",
+            paddingTop:"25px",
+            paddingBottom:"25px",
+          }}
+        >
+          <Stack
+            spacing={5}
+            direction="row"
+            sx={{
+              justifyContent: "center",
+              alignItems: "center",
+              display: "flex",
+              flexFlow: "row",
+            }}
+          >
+            <Box>
+              <Stack>
+                <Typography
+                  component="h1"
+                  fontFamily="Nunito"
+                  align="center"
+                  color="#1E1E1E"
+                  fontSize="25px"
+                  gutterBottom
+                  fontWeight="800"
+                  marginBottom="15px"
+                >
+                  РЕЄСТРУЙТЕ ПОДІЮ ТУТ!
+                </Typography>
+                <Stack
+                  direction="row"
+                  spacing={2}
+                  justifyContent="center"
+                  marginBottom={2}
+                >
+                  <TextField
+                    required
+                    id="outlined-required"
+                    label="Назва події"
+                    defaultValue="Велосотка 2022"
+                  />
+                  <TextField
+                    required
+                    id="outlined-required"
+                    label="Організатор"
+                    defaultValue="UnionRun"
+                  />
+                </Stack>
+              </Stack>
+
+              <Stack direction="row" spacing={2} justifyContent="center">
+                <TextField
+                  required
+                  id="outlined-required"
+                  label="Required"
+                  defaultValue="Hello World"
+                />
+                <TextField
+                  required
+                  id="outlined-required"
+                  label="Required"
+                  defaultValue="Hello World"
+                />
+              </Stack>
+            </Box>
+
+            <Box
+              sx={{
+                width: 300,
+                height: 300,
+                backgroundColor: "yellow",
+              }}
+            />
+          </Stack>
+        </Box>
       </Box>
-      <Box align="center">
-       <Box
-        sx={{ flexGrow: 1, "& .MuiTextField-root": { m: 1, width: "25ch" } }}
-      >
-        <Grid container spacing={2} columns={16}>
-          <Grid xs={6}>
-          <h1 align="center">Про захід</h1>
-            <TextField
-              required
-              fullWidth
-              id="outlined-required"
-              label="Назва заходу"
-              defaultValue="Назва заходу"
-            />
-            <TextField
-              required
-              fullWidth
-              id="outlined-required"
-              label="Посилання на форму"
-              defaultValue="Посилання на форму"
-            />
-            <TextField
-              required
-              id="outlined-required"
-              label="Номер телефону"
-              defaultValue="+380345345243"
-            />
-            <TextField
-              required
-              id="outlined-required"
-              label="Організатор"
-              defaultValue="Організатор"
-            />
-          </Grid>
-          <Grid xs={10}>
-            <TextField
-              multiline={true}
-              rows={10}
-              required
-              id="outlined-required"
-              label="Required"
-              defaultValue="Hello World"
-              style={{ width: "55ch" }}
-            />
-          </Grid>
-        </Grid>
-      </Box> 
-      </Box>
-      
+
       <Footer />
     </Box>
   );
