e68d540a7d658af47a3b9694278b659e4b64e341
Mark Koval <marekmark22gmail.com>
2023-04-03 12:49:43 +0300

    modified:   src/pages/test/Test.jsx

--- src/pages/test/Test.jsx
+++ src/pages/test/Test.jsx
@@ -1,12 +1,10 @@
 import * as React from "react";
 import Box from "@mui/material/Box";
 import Typography from "@mui/material/Typography";
-import TextField from "@mui/material/TextField";
 import Footer from "../../modules/shared/components/Footer";
 import { LineBox } from "../../modules/shared/components/LineBox";
-import Stack from "@mui/material/Stack";
 import { useForm } from "react-hook-form";
-import { Input, OutlinedInput } from "@mui/material";
+import { Grid, OutlinedInput, Paper, styled } from "@mui/material";
 
 export default function Test() {
   const {
@@ -16,6 +14,14 @@ export default function Test() {
   } = useForm();
   const onSubmit = (data) => console.log(data);
   console.log(errors);
+
+  const Item = styled(Paper)(({ theme }) => ({
+    backgroundColor: theme.palette.mode === "dark" ? "#1A2027" : "#fff",
+    ...theme.typography.body2,
+    padding: theme.spacing(1),
+    textAlign: "center",
+    color: theme.palette.text.secondary,
+  }));
 
   return (
     <Box>
@@ -87,136 +93,108 @@ export default function Test() {
       >
         <Box
           sx={{
-            width: "58%",
+            width: "60%",
             backgroundImage:
-              "linear-gradient(180deg, rgba(191,198,227,0) 0%, rgba(191,198,227,1) 20%, rgba(191,198,227,1) 80%, rgba(191,198,227,0) 100%)",
+              "linear-gradient(180deg, rgba(191,198,227,0) 0%, rgba(191,198,227,1) 8%, rgba(191,198,227,1) 95%, rgba(191,198,227,0) 100%)",
             justifyContent: "center",
             display: "flex",
             borderRadius: "25px",
-            marginTop: "25px",
+            marginTop: "45px",
             marginBottom: "25px",
             paddingTop: "25px",
             paddingBottom: "25px",
           }}
         >
-            <form onSubmit={handleSubmit(onSubmit)} sx={{
-              marginBottom:"20px",
-              marginTop: "20px",
-              justifyContent: "center",
-            }}>
-              <OutlinedInput
-                sx={{
-                  marginBottom: "20px",
-                  marginTop: "20px",
-                  marginLeft: "20px",
-                  marginRight: "20px",
-                }}
-                type="text"
-                placeholder="First name"
-                {...register("First name", { required: true, maxLength: 80 })}
-              />
-              <OutlinedInput
-                sx={{
-                  marginBottom: "20px",
-                  marginTop: "20px",
-                  marginLeft: "20px",
-                  marginRight: "20px",
-                }}
-                type="text"
-                placeholder="Last name"
-                {...register("Last name", { required: true, maxLength: 100 })}
-              />
-              <OutlinedInput
-                sx={{
-                  marginBottom: "20px",
-                  marginTop: "20px",
-                  marginLeft: "20px",
-                  marginRight: "20px",
-                }}
-                type="text"
-                placeholder="Email"
-                {...register("Email", {
-                  required: true,
-                  pattern: /^\S+@\S+$/i,
-                })}
-              />
-              <OutlinedInput
-                sx={{
-                  marginBottom: "20px",
-                  marginTop: "20px",
-                  marginLeft: "20px",
-                  marginRight: "20px",
-                }}
-                type="tel"
-                placeholder="Mobile number"
-                {...register("Mobile number", {
-                  required: true,
-                  minLength: 6,
-                  maxLength: 12,
-                })}
-              />
-              <OutlinedInput
-                sx={{
-                  marginBottom: "20px",
-                  marginTop: "20px",
-                  marginLeft: "20px",
-                  marginRight: "20px",
-                }}
-                type="text"
-                placeholder="Event name"
-                {...register("Event name", {})}
-              />
-              <OutlinedInput
-                sx={{
-                  marginBottom: "20px",
-                  marginTop: "20px",
-                  marginLeft: "20px",
-                  marginRight: "20px",
-                }}
-                type="datetime"
-                placeholder="Event date"
-                {...register("Event date", {})}
-              />
-              <OutlinedInput
-                sx={{
-                  marginBottom: "20px",
-                  marginTop: "20px",
-                  marginLeft: "20px",
-                  marginRight: "20px",
-                }}
-                type="text"
-                placeholder="Event category"
-                {...register("Event category", {})}
-              />
-              <OutlinedInput
-                sx={{
-                  marginBottom: "20px",
-                  marginTop: "20px",
-                  marginLeft: "20px",
-                  marginRight: "20px",
-                }}
-                type="number"
-                placeholder="Number of people"
-                {...register("Number of people", {})}
-              />
-              <OutlinedInput
-                sx={{
-                  marginBottom: "20px",
-                  marginTop: "20px",
-                  marginLeft: "20px",
-                  marginRight: "20px",
-                }}
-                type="text"
-                placeholder="About event"
-                {...register("About event", {})}
-              />
-
-              <OutlinedInput sx={{
-
-              }} type="submit" />
-            </form>
+          <form onSubmit={handleSubmit(onSubmit)}>
+            <Grid container spacing={2} justifyContent="center">
+              <Grid item xs={6} md={5}>
+                <OutlinedInput
+                  fullWidth
+                  type="text"
+                  placeholder="First name"
+                  {...register("First name", { required: true, maxLength: 80 })}
+                />
+              </Grid>
+              <Grid item xs={6} md={5}>
+                <OutlinedInput
+                  fullWidth
+                  type="text"
+                  placeholder="Last name"
+                  {...register("Last name", { required: true, maxLength: 100 })}
+                />
+              </Grid>
+              <Grid item xs={6} md={5}>
+                <OutlinedInput
+                  fullWidth
+                  type="text"
+                  placeholder="Email"
+                  {...register("Email", {
+                    required: true,
+                    pattern: /^\S+@\S+$/i,
+                  })}
+                />
+              </Grid>
+              <Grid item xs={6} md={5}>
+                <OutlinedInput
+                  fullWidth
+                  type="tel"
+                  placeholder="Mobile number"
+                  {...register("Mobile number", {
+                    required: true,
+                    minLength: 6,
+                    maxLength: 12,
+                  })}
+                />
+              </Grid>
+              <Grid item xs={6} md={6}>
+                <OutlinedInput
+                  fullWidth
+                  type="text"
+                  placeholder="Event name"
+                  {...register("Event name", {})}
+                />
+              </Grid>
+              <Grid item xs={6} md={6}>
+                <OutlinedInput
+                  fullWidth
+                  type="datetime"
+                  placeholder="Event date"
+                  {...register("Event date", {})}
+                />
+              </Grid>
+              <Grid item xs={6} md={6}>
+                <OutlinedInput
+                  fullWidth
+                  type="text"
+                  placeholder="Event category"
+                  {...register("Event category", {})}
+                />
+              </Grid>
+              <Grid item xs={6} md={6}>
+                <OutlinedInput
+                  fullWidth
+                  type="number"
+                  placeholder="Number of people"
+                  {...register("Number of people", {})}
+                />
+              </Grid>
+              <Grid item xs={6} md={6}>
+                <OutlinedInput
+                  fullWidth
+                  type="text"
+                  placeholder="About event"
+                  {...register("About event", {})}
+                />
+              </Grid>
+              <Grid item xs={6} md={6}>
+                <OutlinedInput fullWidth type="submit" />
+              </Grid>
+            </Grid>
+          </form>
         </Box>
       </Box>
+
+      {/* Breakpoint */}
 
       <Footer />
     </Box>
